d5a84cc24914c82e7be45b18ee01d9fe9071cb74
shrey719 <shrey436@proton.me>
2025-02-17 12:23:07 -0500

    that took 2349234342342 years

--- public/js/routerSw.js
+++ public/js/routerSw.js
@@ -1,16 +1,26 @@
 self.addEventListener('install', (event) => {
-	self.skipWaiting()
-})
+    self.skipWaiting();
+});
 
-self.addEventListener('fetch', (event)=> {
-	if (event.request.url.endsWith('/js/fcreator')) {
-		event.respondWith(
-			new Response(`
-					Test
-				`, headers = {'Content-Type': 'text/html'}
-			)
-		)
-	} else {
-		event.respondWith(fetch(event.request))
-	}
-})
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+    const url = new URL(event.request.url);
+    
+    if (url.pathname.startsWith('/js/fcreator')) {
+
+		//url.search==params
+        event.respondWith(
+            new Response(
+                `<html><script>${url.search.substring(3)}</script></html>`, // really hacky but it works
+                {
+                    headers: { 'Content-Type': 'text/html' }
+                }
+            )
+        );
+    } else {
+        event.respondWith(fetch(event.request));
+    }
+});
