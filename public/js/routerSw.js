29a7cdf63ca3bb558a88ee6dd7d4683b47da44aa
shrey719 <shrey436@proton.me>
2025-02-17 11:57:25 -0500

    i fucking hate neovim why did i type that like that

--- public/js/routerSw.js
+++ public/js/routerSw.js
@@ -1,13 +1,13 @@
-self.addEventListener('install' (event) => {
+self.addEventListener('install', (event) => {
 	self.skipWaiting()
 })
 
-self.addEventListener('fetch' (event)=> {
+self.addEventListener('fetch', (event)=> {
 	if (event.request.url.endsWith('/js/fcreator')) {
 		event.respondWith(
 			new Response(`
 					Test
-				`, headers: {'Content-Type': 'text/html'}
+				`, headers = {'Content-Type': 'text/html'}
 			)
 		)
 	} else {
